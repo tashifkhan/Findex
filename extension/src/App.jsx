7dfe5cb82c6ea32d4d78dce7b3bffd32f8790ee0
unknown <saarthakagarwal0408@gmail.com>
2025-07-12 20:58:43 +0530

    Fixing errors

--- extension/src/App.jsx
+++ extension/src/App.jsx
@@ -2,6 +2,8 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import ChatSidebar from './components/ChatSidebar'
+import SidebarController from './components/YouTubeQAApp'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -9,7 +11,7 @@ function App() {
   return (
     <>
       <div>
-        Hello world!!
+        <SidebarController />
       </div>
     </>
   )
