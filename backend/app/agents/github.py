bf3e1fce35a99439700e3036ab258e8cb8f0680c
Tashif Ahmad Khan <tashifkhan010@gmail.com>
2025-07-26 19:32:58 +0530

    added gihub tool

---
+++ backend/app/agents/github.py
@@ -0,0 +1,16 @@
+from langchain_community.tools import tool
+from pydantic import HttpUrl
+from app.routes.git_crawl import generate_github_answer
+
+
+@tool
+async def github_agent(url: HttpUrl, question: str) -> str:
+    """GitHub Tool for getting response using GitHub repository link and stuff, like provide the url of the GitHub repository you need information for and the relevant question, it would return the answer from the repository only, if data is not available it would return a message saying that data is not available."""
+
+    res = await generate_github_answer(
+        url=url,
+        question=question,
+        chat_history=[],
+    )
+
+    return res
