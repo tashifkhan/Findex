87128174d8c50acfaaac726490331b35c67602b2
Tashif Ahmad Khan <tashifkhan010@gmail.com>
2025-07-26 19:31:03 +0530

    toool edit

--- backend/app/agents/webite.py
+++ backend/app/agents/webite.py
@@ -5,7 +5,7 @@ from app.routes.website import generate_website_answer
 
 @tool
 async def website_agent(url: HttpUrl, question: str) -> str:
-    """Website Agent for getting response using website link and stuff, like provide the url of the website you need information for and the relevant question, it would return the answer from the website only, if data is not available it would return a message saying that data is not available."""
+    """Website Tool for getting response using website link and stuff, like provide the url of the website you need information for and the relevant question, it would return the answer from the website only, if data is not available it would return a message saying that data is not available."""
 
     res = await generate_website_answer(
         url=str(url),
--- backend/app/agents/youtube.py
+++ backend/app/agents/youtube.py
@@ -5,7 +5,7 @@ from app.routes.ask import generate_answer as get_youtube_answer
 
 @tool
 async def youtube_agent(url: HttpUrl, question: str) -> str:
-    """YouTube Agent for getting response using youtube link and stuff, like provide the url of the youtube video you need information for and the relevant question, it would return the answer from the video only, if data is not available it would return a message saying that data is not available."""
+    """YouTube Tool for getting response using youtube link and stuff, like provide the url of the youtube video you need information for and the relevant question, it would return the answer from the video only, if data is not available it would return a message saying that data is not available."""
 
     res = await get_youtube_answer(
         url=str(url),
