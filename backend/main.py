9f4373ab0252b1a9ed89ce4c7114f1257164d0fc
Tashif Ahmad Khan <tashifkhan010@gmail.com>
2025-07-29 11:39:35 +0530

    deploying server

--- backend/main.py
+++ backend/main.py
@@ -1,23 +1,5 @@
 from app.main import mainfn as server
-import asyncio
-from app.agents.agent import GraphBuilder
 
-async def main():
-    graph = GraphBuilder()()
-    
-    messages = {
-        "messages": ["hello"]
-    }
-
-    output = await graph.ainvoke(messages)
-
-    if isinstance(output, dict) and "messages" in output:
-        final_output = output["messages"][-1].content  
-    else:
-        final_output = str(output)
-
-    print(final_output)
 
 if __name__ == "__main__":
-    asyncio.run(main())
-
+    server()
